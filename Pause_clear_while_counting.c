f95f72816d61c70c41b39b1006544fec303ffb81
shehab-Mohamed-Hosni <105604404+shehab-Mohamed-Hosni@users.noreply.github.com>
2022-05-23 04:23:11 +0200

    Add files via upload

---
+++ Pause_clear_while_counting.c
@@ -0,0 +1,23 @@
+void pause_cont_clear(){
+	while(sw1_input()==0 | sw3_input()==0 )
+			{
+while(1){
+	GPIOF->DATA &=~0x0E ;
+			delayMs(1000) ;
+			GPIOF->DATA |=0x0E ;
+			delayMs(1000) ;	
+	//delayMs(500);
+while(sw2_input()==0) {count_down(h,j,k,y);};
+
+while(sw1_input()==0){
+				LCD_command(0x01);
+			count_down(0,0,0,0);
+			GPIOF->DATA &=~0x0E ;
+//h=0;j=0; k=0;y=0;
+	}
+
+}
+
+}
+}
+
